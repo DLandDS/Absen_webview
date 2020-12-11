0a7e4156876b2a9db33ba3fdca807ca254c6ab9f
Taufiqurahman <kimsoohyun1344@gmail.com>
2020-12-11 20:44:12 +0700

    Simple Webview

--- app/src/main/java/org/dlands/eabsen/MainActivity.java
+++ app/src/main/java/org/dlands/eabsen/MainActivity.java
@@ -3,6 +3,8 @@ package org.dlands.eabsen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://kegiatan.iainbukittinggi.ac.id/absen/");
     }
 }
